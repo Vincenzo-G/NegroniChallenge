bcadd30ae6007fc058dcb4f7ab0389dbbee3ca7a
giulia892 <giloffredo02@gmail.com>
2024-11-11 10:54:05 +0100

    giulia

--- NegroniChallenge/ContentView.swift
+++ NegroniChallenge/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("Faz was here")
+            Text("Giulia està aquì")
         }
         .padding()
     }
