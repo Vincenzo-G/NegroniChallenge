d9623a5097f594e10f0ef7f0b111eeb16beb71d4
Vincenzo-G <vincenzogerelli@gmail.com>
2024-11-11 09:33:26 +0100

    Initial Commit

---
+++ NegroniChallenge/NegroniChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegroniChallengeApp.swift
+//  NegroniChallenge
+//
+//  Created by Vincenzo Gerelli on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NegroniChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
